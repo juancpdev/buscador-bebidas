bfab9d8bf9656d2ab2b072ea0c79666ada7fa7f9
Juan Cruz <juan_pineda_12@hotmail.es>
2024-10-29 16:35:43 -0300

    card styles

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -6,22 +6,28 @@ type DrinkCardProp = {
 
 export default function DrinkCard({drink}: DrinkCardProp) {
   return (
-    <div className="border shadow-md text-center rounded-lg">
-        <p className="p-3 text-md font-bold">{drink.strDrink}</p>
-        <div className=" overflow-hidden">
-            <img 
-                src={drink.strDrinkThumb} 
-                alt={`Imagen de ${drink.strDrink}`}
-                className="hover:scale-125 hover:rotate-3 transition-all"
-            />
+    <div className="flex flex-col h-full text-center rounded-lg bg-gray-100 shadow-card">
+      
+        <p className="p-3 text-md font-bold line-clamp-2 min-h-[4rem] flex items-center justify-center">
+          {drink.strDrink}
+        </p>
+        
+        <div className="flex-grow overflow-hidden">
+          <img 
+            src={drink.strDrinkThumb} 
+            alt={`Imagen de ${drink.strDrink}`}
+            className="object-cover w-full h-full hover:scale-125 hover:rotate-3 transition-all"
+          />
         </div>
-        <button
-            type="button"
-            className="bg-gradient-to-tr from-orange-500 to-yellow-300 text-black w-full font-bold p-3 rounded-b-lg 
-            hover:bg-gradient-to-tl hover:from-orange-300 hover:to-yellow-100 transition-all"
-        >
-            Ver Receta
-        </button>
+      
+
+      <button
+        type="button"
+        className="bg-gradient-to-tr from-orange-500 to-yellow-300 text-black w-full font-bold p-3 mt-auto 
+        rounded-b-lg hover:bg-gradient-to-tl hover:from-orange-300 hover:to-yellow-100 transition-all"
+      >
+        Ver Receta
+      </button>
     </div>
    
   )
--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -11,9 +11,9 @@ export default function IndexPage() {
 
     return (
       <>
-        <h2 className="text-5xl font-bold mb-10 text-center">Drinks</h2>
+        <h2 className="text-5xl font-bold mb-16 text-center">Drinks</h2>
         {hasDrinks ? (
-          <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-10 m-4">
+          <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5 md:gap-10 m-4 md:mx-10 xl:mx-40">
             {drinks.drinks.map(drink => (
               <DrinkCard
                 key={drink.idDrink}
